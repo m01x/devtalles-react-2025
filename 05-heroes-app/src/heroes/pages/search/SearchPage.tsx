8ad6cd92301d8e6848fb73cc08e8625f9e8f7a31
m01x <fortiz.informatica@gmail.com>
2026-03-09 21:06:37 -0300

    feat: Implement initial application structure, routing, hero layouts, and foundational UI components.

--- 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -1,3 +1,14 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      <CustomJumbotron
+        title="Busqueda de Super Heroe"
+        description="Ingresa algún parámetro"
+      />
+    </>
+  );
 };
+
+export default SearchPage;
